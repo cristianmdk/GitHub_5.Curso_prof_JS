28719167f2e762b717c28051563a107060f4c834
Cristian Angulo <cristianmdk@hotmail.com>
2020-06-16 17:01:17 +0700

    intersectionObserver

---
+++ V21_IntersectionObserver/assets/index.js
@@ -0,0 +1,20 @@
+import MediaPlayer from './MediaPlayer.js'
+import {MyVar} from './MediaPlayer.js' //to export a var i have to do it with the same name
+import AutoPlay from './plugins/AutoPlay.js'
+import AutoPause from './plugins/AutoPause.js'
+
+
+const video = document.querySelector('video');
+const player = new MediaPlayer({ 
+    el: video,
+    plugins: [
+        new AutoPlay(),
+        new AutoPause()
+    ],
+});
+
+const button = document.querySelector('#playButton');
+button.onclick = () => player.togglePlay(); 
+
+const muteButton = document.querySelector('#muteButton');
+muteButton.onclick = () => player.mute_unmute();
